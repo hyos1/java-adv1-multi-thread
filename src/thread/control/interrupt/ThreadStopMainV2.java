2198793d22eaa535201758eba46414d3e6d084ab
hyos <hyozzang99@gmail.com>
2026-07-08 17:21:16 +0900

    시간 순으로 인터럽트 상태 확인하는 로그 추가

--- src/thread/control/interrupt/ThreadStopMainV2.java
+++ src/thread/control/interrupt/ThreadStopMainV2.java
@@ -23,7 +23,7 @@ public class ThreadStopMainV2 {
             while (!Thread.currentThread().isInterrupted()) {
                 log("작업 중");
             }
-            log("work 스레드 인터럽트 상태1 = " + Thread.currentThread().isInterrupted());
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
 
             try {
                 log("자원 정리");
@@ -31,6 +31,7 @@ public class ThreadStopMainV2 {
                 log("자원 종료");
             } catch (InterruptedException e) {
                 log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted());
             }
         }
     }
