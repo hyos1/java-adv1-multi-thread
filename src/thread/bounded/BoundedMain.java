2587c3851284c380ae6dcf4df85f03df285ff6e4
hyos <hyozzang99@gmail.com>
2026-07-14 16:24:19 +0900

    V3 생산자 소비자 버퍼 문제 확인 - wait()로 스레드 상태를 WAITING으로 만들고, 락을 놓는다. - 작업이 끝난 후 WAITING상태의 스레드를 notify(), notifyAll()로 깨운다.

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -15,7 +15,8 @@ public class BoundedMain {
     public static void main(String[] args) {
         // 1. BoundedQueue
 //        BoundedQueue queue = new BoundedQueueV1(2);
-        BoundedQueue queue = new BoundedQueueV2(2);
+//        BoundedQueue queue = new BoundedQueueV2(2);
+        BoundedQueue queue = new BoundedQueueV3(2);
 
         // 2. 생산자 소비가 실행 순서 선택, 반드시 하나만 선택
 //        producerFirst(queue); // 생산자 먼저 실행
